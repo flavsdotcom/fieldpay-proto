b98842d25cf800bb6b1f9fe870214306a40595d8
Flavia I de la Fuente <flaviaidelafuente@Flavias-MacBook-Pro.local>
2025-03-06 22:31:38 -0600

    Fixed Recruiting Pane Navigation

--- src/App.js
+++ src/App.js
@@ -30,11 +30,25 @@ const App = () => {
   return (
     <div>
       {activePage === "home" && <AdminHome setActivePage={setActivePage} />}
+      {activePage === "recruiting" && <Recruiting setActivePage={setActivePage} />}
       {activePage === "teamwork" && <Teamwork setActivePage={setActivePage} />}
       {activePage === "coverage-request" && <CreateCoverageRequest setActivePage={setActivePage} requests={requests} setRequests={setRequests} />}
       {activePage === "active-requests" && <ActiveRequests setActivePage={setActivePage} requests={requests} />}
       {activePage === "ride-along-bonus" && <CreateRideAlongBonus setActivePage={setActivePage} />}
       {activePage === "referral-bonus" && <CreateReferralBonus setActivePage={setActivePage} />}
+    </div>
+  );
+};
+
+const Recruiting = ({ setActivePage }) => {
+  return (
+    <div className="app-container">
+      <h2 className="top-third">Recruiting</h2>
+      <p className="info-text">High performers want to work with other high performers. Relationships at work drive retention.</p>
+      <Button className="custom-button" onClick={() => setActivePage("referral-bonus")}>Create Referral Bonus</Button>
+      <p className="info-text">De-risk hiring by inviting a candidate to “ride along” with a team member. Evaluate their ability to learn and their composure in customer environments.</p>
+      <Button className="custom-button" onClick={() => setActivePage("ride-along-bonus")}>Create Ride Along Bonus</Button>
+      <Button className="custom-button" onClick={() => setActivePage("home")}>Home</Button>
     </div>
   );
 };
