2f9fc7e92d6024062c8ee092bd0d2592fcd90328
Flavia I de la Fuente <flaviaidelafuente@Flavias-MacBook-Pro.local>
2025-03-07 21:38:25 -0600

    Added Recruiting component

---
+++ src/Recruiting.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const Recruiting = ({ setActivePage }) => {
+  return (
+    <div className="app-container">
+      <h2 className="top-third">Recruiting</h2>
+      <p className="info-text">
+        High performers want to work with other high performers. Relationships at work drive retention.
+      </p>
+      <Button className="custom-button" onClick={() => setActivePage("referral-bonus")}>
+        Create Referral Bonus
+      </Button>
+      <p className="info-text">
+        De-risk hiring by inviting a candidate to “ride along” with a team member. Evaluate their ability to learn and their composure in a customer environment.
+      </p>
+      <Button className="custom-button" onClick={() => setActivePage("ride-along-bonus")}>
+        Create Ride Along Bonus
+      </Button>
+      <Button className="custom-button" onClick={() => setActivePage("home")}>
+        Home
+      </Button>
+    </div>
+  );
+};
+
+export default Recruiting;
+
